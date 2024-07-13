c55e8da93151a466563a4d743af3b50b2acb90a8
“[Malik <[malikumair.dev@gmail.com]>
2024-07-13 12:47:18 -0700

    Loops Completed

---
+++ Core Concepts of JavaScript/loops.js
@@ -0,0 +1,14 @@
+// while loop
+
+let counter = 0;
+
+while (counter < 10) {
+  console.log(counter + 1);
+  counter++;
+}
+
+//for loop
+console.log("for loop");
+for (let i = 0; i < 10; i++) {
+  console.log(i + 1);
+}
