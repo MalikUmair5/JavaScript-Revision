35a329a29df0a06e9f4600c6d7f9ff10902bac95
[Malik Umair] <[malikumair.dev@gmail.com]>
2024-07-13 17:22:58 +0500

    strings

---
+++ Core Concepts of JavaScript/string.js
@@ -0,0 +1,12 @@
+//? Double quote String
+let str1 = "My name is Umair"
+console.log(str1)
+
+//? Single quote string
+let str2 = 'My age is 22'
+console.log(str2)
+
+//? template literals backticks
+let str3 = `${str1} & ${str2}`
+console.log(str3)
+
