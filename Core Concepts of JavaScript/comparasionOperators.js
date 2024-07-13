8d0c538be49da44e99750c550f89421c0dcdb593
[Malik Umair] <[malikumair.dev@gmail.com]>
2024-07-13 17:56:22 +0500

    Comparasion Operators in JS

---
+++ Core Concepts of JavaScript/comparasionOperators.js
@@ -0,0 +1,18 @@
+
+
+//? Equality Operators
+
+console.log(4 == '4')
+console.log (4 === '4')
+
+//? Inequality Operators
+
+console.log(4 != '4')
+console.log (4 !== '4')
+
+//? Relational Operators
+
+console.log(4 > 8)
+console.log (4 < 8)
+console.log(8 <= 8)
+console.log(4 >= 4)
