37ee69f83d2b76b4008f17e7698e5626c6d45e1c
[Malik Umair] <[malikumair.dev@gmail.com]>
2024-07-13 17:45:21 +0500

    typeof Operator in JS

---
+++ Core Concepts of JavaScript/typeOfOperator.js
@@ -0,0 +1,9 @@
+let userName;
+
+console.log(`type of 4 is ${typeof 4}`);
+console.log(`type of hello is ${typeof 'hello'}`);
+console.log(`type of 4.5 is ${typeof 4.5}`);
+console.log(`type of true is ${typeof true}`);
+console.log(`type of false is ${typeof false}`);
+console.log(`type of null is ${typeof null}`);
+console.log(`type of userName is ${typeof userName}`);
