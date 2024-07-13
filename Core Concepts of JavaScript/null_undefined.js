3c31958b99fc3e5e8e636a19410e91cceeda446e
[Malik Umair] <[malikumair.dev@gmail.com]>
2024-07-13 17:38:29 +0500

    Boolean in JS

---
+++ Core Concepts of JavaScript/null_undefined.js
@@ -0,0 +1,15 @@
+
+//? underfined is value that comes if we declare a variable but donot assign the value
+let your_name;
+console.log(`your name is ${your_name}`)
+your_name = "Umair"
+console.log(`your name is ${your_name}`)
+
+//? Null value is a value that is assigned or used explicitely
+
+let my_name = null
+console.log(`my name is ${my_name}`)
+my_name = "khan"
+console.log(`my name is ${my_name}`)
+
+
