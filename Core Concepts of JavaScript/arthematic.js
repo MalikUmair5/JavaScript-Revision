a8ec65fd31f3e8012b327c1ddd7e1718b96ff55e
[Malik Umair] <[malikumair.dev@gmail.com]>
2024-07-13 15:46:51 +0500

    Arthematic Operators & Variables

---
+++ Core Concepts of JavaScript/arthematic.js
@@ -0,0 +1,11 @@
+
+
+//? Understanding Arthematic Operators in JavaScript
+
+console.log(5+2)
+console.log(5-2)
+console.log(5*2)
+console.log(5/2)
+
+//? Give the reminder of the Devision
+console.log(5%2)
---
+++ Core Concepts of JavaScript/variables.js
@@ -0,0 +1,19 @@
+let money = 100;
+console.log(money);
+
+let money2 = 45;
+console.log(money2);
+money2 = money + 40;
+console.log(money2);
+
+let noOfStudents = 45;
+noOfStudents += 5;
+console.log(noOfStudents); //50
+noOfStudents /= 5;
+console.log(noOfStudents); //10
+
+noOfStudents *= 5;
+console.log(noOfStudents); //50
+
+noOfStudents -= 5;
+console.log(noOfStudents); //45
