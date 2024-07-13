f1ab42017c9772f727c82ada318c226f3a0c3193
[Malik Umair] <[malikumair.dev@gmail.com]>
2024-07-13 15:27:25 +0500

    JavaScript Introduction

---
+++ Introduction to JavaScript/HistoryOfJavaScript.js
@@ -0,0 +1,4 @@
+// First Named Mocha then LiveScript after that finally name JavaScript
+// Named after popularity of Java at that time
+// JavaScript was created in 1995
+// it is intepreted language, it is compiled line by line. 
---
+++ Introduction to JavaScript/whatIsJavaScript.js
@@ -0,0 +1,5 @@
+// JavaScript is a High-level, dynamic programming language.
+// Actions: Enables Interactivity
+// Updates: Alter page without reloading
+// Events: Responds to user action
+// Data: Fetches and Sends info to server
