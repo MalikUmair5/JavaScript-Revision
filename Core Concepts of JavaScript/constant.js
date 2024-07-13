9e72e1e85a11b8542a39710b012aaa2d72ed8c0f
[Malik Umair] <[malikumair.dev@gmail.com]>
2024-07-13 16:45:27 +0500

    Constant Variable const

---
+++ Core Concepts of JavaScript/constant.js
@@ -0,0 +1,14 @@
+let radius = 4;
+
+const pi = 3.142; // pi is a constant value it can not be changed
+
+let area = pi * radius * radius;
+
+console.log(area)
+
+
+//! No one can change the value of pi following code will throw an error
+// pi = 98;
+
+let perimeter = 2 * pi * radius;
+console.log (radius)
