d2343fbd117c2d44ddf37ee4240a99cc84fae857
“[Malik <[malikumair.dev@gmail.com]>
2024-07-14 21:24:19 -0700

    Understanding Arrays

--- Advance Topics of JavaScript/arrays.js
+++ Advance Topics of JavaScript/arrays.js
@@ -4,4 +4,10 @@ console.log(arr);
 console.log(arr[0])
 console.log(arr[3])
 console.log(arr[4])
-console.log(arr[5]) 
+console.log(arr[5]) //? undefined
+
+console.log(arr.length)
+
+for(let i = 0; i < arr.length; i++){
+    console.log(arr[i]);
+}
